4c5aba39100ce814452b424cd699e7a56f7639ed
kanghyonghyun <hmoon826@gmail.com>
2022-09-13 23:54:08 +0900

    init

---
+++ src/main/java/com/tennisFriends/TennisFriendsApplication.java
@@ -0,0 +1,13 @@
+package com.tennisFriends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisFriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisFriendsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tennisFriends/TennisFriendsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tennisFriends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisFriendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
