cec2d07b06aefca6100a5c9c6c04e4900ca4f40e
kanghyonghyun <hmoon826@gmail.com>
2022-09-14 23:54:05 +0900

    [feat] 계정 도메인 생성

---
+++ src/main/java/com/tennisFriends/domain/Account.java
@@ -0,0 +1,52 @@
+package com.tennisFriends.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter @EqualsAndHashCode(of = "id")
+@Builder @AllArgsConstructor @NoArgsConstructor
+public class Account {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String nickname;
+
+    private String password;
+
+    private boolean emailVerified;
+
+    private String emailCheckToken;
+
+    private LocalDateTime joinAt;
+
+    private String bio;
+
+    private String url;
+
+    private String occupation;
+
+    private String location;
+
+    @Lob @Basic(fetch = FetchType.EAGER)
+    private String profileImage;
+
+    private boolean friendCreatedByEmail;
+    private boolean friendCreatedByWeb;
+
+    private boolean friendEnrollmentResultByEmail;
+    private boolean friendEnrollmentResultByWeb;
+
+    private boolean friendUpdatedByEmail;
+    private boolean friendUpdatedByWeb;
+
+
+}
