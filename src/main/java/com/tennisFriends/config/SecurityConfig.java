ba34d329a6f0905c90508290a170c0de47133a24
kanghyonghyun <hmoon826@gmail.com>
2022-09-19 23:33:00 +0900

    [add] security config 설정 추가 - static resource 인증 제거

--- src/main/java/com/tennisFriends/config/SecurityConfig.java
+++ src/main/java/com/tennisFriends/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.tennisFriends.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,5 +20,10 @@ public class SecurityConfig {
                 .mvcMatchers(HttpMethod.GET, "/profile/*").permitAll()
                 .anyRequest().authenticated();
         return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 }
