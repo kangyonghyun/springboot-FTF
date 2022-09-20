7947b996ad17a74c9c03ea98a59bc6625d233746
kanghyonghyun <hmoon826@gmail.com>
2022-09-21 00:49:03 +0900

    [add] 회원가입 form 검증 및 메일인증 추가

--- src/main/java/com/tennisFriends/domain/Account.java
+++ src/main/java/com/tennisFriends/domain/Account.java
@@ -4,6 +4,7 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
 @Getter @Setter @EqualsAndHashCode(of = "id")
@@ -49,4 +50,7 @@ public class Account {
     private boolean friendUpdatedByWeb;
 
 
+    public void generateEmailCheckToken() {
+        this.emailCheckToken = UUID.randomUUID().toString();
+    }
 }
