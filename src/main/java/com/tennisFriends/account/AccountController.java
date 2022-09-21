5d6531f33f7e250f5387803ce41da6618b3591cd
kanghyonghyun <hmoon826@gmail.com>
2022-09-21 23:56:04 +0900

    [refactor] 회원가입 controller 에서 service 로 로직 이동

--- src/main/java/com/tennisFriends/account/AccountController.java
+++ src/main/java/com/tennisFriends/account/AccountController.java
@@ -20,8 +20,7 @@ import javax.validation.Valid;
 public class AccountController {
 
     private final SignUpFormValidator signUpFormValidator;
-    private final AccountRepository accountRepository;
-    private final JavaMailSender javaMailSender;
+    private final AccountService accountService;
 
     @InitBinder("signUpForm")
     public void initBinder(WebDataBinder webDataBinder) {
@@ -39,23 +38,7 @@ public class AccountController {
         if (errors.hasErrors()) {
             return "account/sign-up";
         }
-        Account account = Account.builder()
-                .email(signUpForm.getEmail())
-                .nickname(signUpForm.getNickname())
-                .password(signUpForm.getPassword()) //TODO encoding 해야함
-                .friendCreatedByWeb(true)
-                .friendEnrollmentResultByWeb(true)
-                .friendUpdatedByWeb(true)
-                .build();
-        Account newAccount = accountRepository.save(account);
-
-        newAccount.generateEmailCheckToken();
-
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setTo(newAccount.getEmail());
-        mailMessage.setSubject("테친찾, 회원 가입 인증");
-        mailMessage.setText("/check-email-token?token=" + newAccount.getEmailCheckToken() + "&email=" + newAccount.getEmail());
-        javaMailSender.send(mailMessage);
+        accountService.processNewAccount(signUpForm);
         return "redirect:/";
     }
 
---
+++ src/main/java/com/tennisFriends/account/AccountService.java
@@ -0,0 +1,42 @@
+package com.tennisFriends.account;
+
+import com.tennisFriends.domain.Account;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AccountService {
+
+    private final AccountRepository accountRepository;
+    private final JavaMailSender javaMailSender;
+
+    public void processNewAccount(SignUpForm signUpForm) {
+        Account newAccount = saveNewAccount(signUpForm);
+        newAccount.generateEmailCheckToken();
+        sendSignUpConfirmEmail(newAccount);
+    }
+
+    private Account saveNewAccount(SignUpForm signUpForm) {
+        Account account = Account.builder()
+                .email(signUpForm.getEmail())
+                .nickname(signUpForm.getNickname())
+                .password(signUpForm.getPassword()) //TODO encoding 해야함
+                .friendCreatedByWeb(true)
+                .friendEnrollmentResultByWeb(true)
+                .friendUpdatedByWeb(true)
+                .build();
+        return accountRepository.save(account);
+    }
+
+    private void sendSignUpConfirmEmail(Account newAccount) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(newAccount.getEmail());
+        mailMessage.setSubject("테친찾, 회원 가입 인증");
+        mailMessage.setText("/check-email-token?token=" + newAccount.getEmailCheckToken() + "&email=" + newAccount.getEmail());
+        javaMailSender.send(mailMessage);
+    }
+
+}
