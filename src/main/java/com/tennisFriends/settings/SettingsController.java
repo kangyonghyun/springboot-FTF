12eb93307a7cf3858d21c007b0dccc8d4dd9a55e
kanghyonghyun <hmoon826@gmail.com>
2022-10-17 13:39:33 +0900

    [feat] 프로필 수정 기능 추가

---
+++ src/main/java/com/tennisFriends/settings/SettingsController.java
@@ -0,0 +1,47 @@
+package com.tennisFriends.settings;
+
+import com.tennisFriends.account.AccountService;
+import com.tennisFriends.account.CurrentUser;
+import com.tennisFriends.domain.Account;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+public class SettingsController {
+
+    private final AccountService accountService;
+    public static final String SETTINGS_PROFILE_URL = "/settings/profile";
+
+    public static final String SETTINGS_PROFILE_VIEW_MAME = "settings/profile";
+
+    @GetMapping(SETTINGS_PROFILE_URL)
+    public String profileUpdateForm(@CurrentUser Account account, Model model) {
+        model.addAttribute(account);
+        model.addAttribute(new Profile(account));
+        return SETTINGS_PROFILE_VIEW_MAME;
+    }
+
+    @PostMapping(SETTINGS_PROFILE_URL)
+    public String updateProfile(@CurrentUser Account account,
+                                @Valid @ModelAttribute Profile profile,
+                                Errors errors, Model model,
+                                RedirectAttributes attributes) {
+        if (errors.hasErrors()) {
+            model.addAttribute(account);
+            return SETTINGS_PROFILE_VIEW_MAME;
+        }
+        accountService.updateProfile(account, profile);
+        attributes.addFlashAttribute("message", "프로필을 수정했습니다.");
+        return "redirect:" + SETTINGS_PROFILE_URL;
+    }
+
+}
