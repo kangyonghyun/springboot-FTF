6c6c7bf5da32e017fac60e16ee54374313ee4dd7
kanghyonghyun <hmoon826@gmail.com>
2022-10-18 14:48:36 +0900

    [refactor] 프로필 DTO 제약조건 추가

--- src/main/java/com/tennisFriends/settings/Profile.java
+++ src/main/java/com/tennisFriends/settings/Profile.java
@@ -3,16 +3,21 @@ package com.tennisFriends.settings;
 import com.tennisFriends.domain.Account;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 @NoArgsConstructor
 public class Profile {
+    @Length(max = 35)
     private String bio;
 
+    @Length(max = 50)
     private String url;
 
+    @Length(max = 50)
     private String occupation;
 
+    @Length(max = 50)
     private String location;
 
     public Profile(Account account) {
