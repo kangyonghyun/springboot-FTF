43535ec5dc8c0f12337e3b399a55ff7a7f4a5eea
kanghyonghyun <hmoon826@gmail.com>
2022-10-25 15:02:26 +0900

    [feat] Zone (지역정보) whitelist 데이터 초기화

---
+++ src/main/java/com/tennisFriends/zone/ZoneRepository.java
@@ -0,0 +1,7 @@
+package com.tennisFriends.zone;
+
+import com.tennisFriends.domain.Zone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ZoneRepository extends JpaRepository<Zone, Long> {
+}
