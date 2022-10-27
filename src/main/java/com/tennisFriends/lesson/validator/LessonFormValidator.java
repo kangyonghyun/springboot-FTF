29bd614f0825469cb222298117ae29854128e376
kanghyonghyun <hmoon826@gmail.com>
2022-10-27 14:34:35 +0900

    [feat] 회원들이 사용할 레슨 form 검증 로직 추가

---
+++ src/main/java/com/tennisFriends/lesson/validator/LessonFormValidator.java
@@ -0,0 +1,28 @@
+package com.tennisFriends.lesson.validator;
+
+import com.tennisFriends.lesson.LessonRepository;
+import com.tennisFriends.lesson.form.LessonForm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+@RequiredArgsConstructor
+public class LessonFormValidator implements Validator {
+
+    private final LessonRepository lessonRepository;
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return LessonForm.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        LessonForm lessonForm = (LessonForm) target;
+        if (lessonRepository.existsByPath(lessonForm.getPath())) {
+            errors.rejectValue("path", "wrong.path", "레슨 경로값을 사용할 수 없습니다.");
+        }
+    }
+}
