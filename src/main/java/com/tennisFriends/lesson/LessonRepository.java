dbd97737319b8b1df840e4750d5458f710b35343
kanghyonghyun <hmoon826@gmail.com>
2022-10-27 14:33:50 +0900

    [feat] 회원들이 사용할 레슨 repository 추가

---
+++ src/main/java/com/tennisFriends/lesson/LessonRepository.java
@@ -0,0 +1,10 @@
+package com.tennisFriends.lesson;
+
+import com.tennisFriends.domain.Lesson;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+public interface LessonRepository extends JpaRepository<Lesson, Long> {
+    boolean existsByPath(String path);
+}
